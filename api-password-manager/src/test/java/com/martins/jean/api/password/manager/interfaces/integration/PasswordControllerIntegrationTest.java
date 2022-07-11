8b6e066d87ec06bb4c3e243f2eb6850c3a5aba57
Jean Martins <jean.martins@sensedia.com>
2022-07-11 09:41:31 -0300

    ajuste na uri do teste de integração do MS

--- api-password-manager/src/test/java/com/martins/jean/api/password/manager/interfaces/integration/PasswordControllerIntegrationTest.java
+++ api-password-manager/src/test/java/com/martins/jean/api/password/manager/interfaces/integration/PasswordControllerIntegrationTest.java
@@ -1,20 +1,21 @@
 package com.martins.jean.api.password.manager.interfaces.integration;
 
 import com.martins.jean.api.password.manager.interfaces.json.request.PasswordRequest;
-import org.junit.jupiter.api.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PasswordControllerIntegration {
+class PasswordControllerIntegrationTest {
 
-    private String baseUrl;
     private URI uri;
 
     @Autowired
@@ -24,13 +25,13 @@ public class PasswordControllerIntegration {
 
     @BeforeEach
     void setUp() throws URISyntaxException {
-        baseUrl = "http://localhost:" + port + "/api/password-manager/v1/verify";
+        String baseUrl = "http://localhost:" + port + "/api/password-manager/v1/verify";
         uri = new URI(baseUrl);
     }
 
     @Test
     @DisplayName("when post verify password and returns password is valid")
-    void post_verifyPassword_ReturnsTrue() throws URISyntaxException {
+    void post_verifyPassword_ReturnsTrue() {
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("AbTp9!fok");
 
@@ -45,7 +46,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password no has  9 Characters")
-    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasNineCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasNineCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("AbTp9!fk");
 
@@ -59,7 +60,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password no has Upper Characters")
-    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasUpperCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasUpperCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("abtp9!fok");
 
@@ -73,7 +74,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password no has Low Characters")
-    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasLowCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasLowCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("ABTP9!FOK");
 
@@ -87,7 +88,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password no has number Characters")
-    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasNumberCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasNumberCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("AbTpm!fok");
 
@@ -101,7 +102,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password no has Character Special ")
-    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasSpecialCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordNotHasSpecialCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("AbTp9lfok");
 
@@ -115,7 +116,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password has same Characters")
-    void post_verifyPassword_ReturnsFalseBecausePasswordHasSameCharacter() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordHasSameCharacter(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("AbTp9!fokA");
 
@@ -129,7 +130,7 @@ public class PasswordControllerIntegration {
 
     @Test
     @DisplayName("when post verify password and returns password is not valid because password has White Space")
-    void post_verifyPassword_ReturnsFalseBecausePasswordHasWhiteSpace() throws URISyntaxException {
+    void post_verifyPassword_ReturnsFalseBecausePasswordHasWhiteSpace(){
         PasswordRequest passwordRequest = getRequestPasswordEmpty();
         passwordRequest.setPassword("Ab9! fok");
 
