4fc83b9ddc15a4afe8b6f219e74abc3de4f4aeb8
Jean Martins <jean.martins@sensedia.com>
2022-07-11 09:33:35 -0300

    ajuste na uri do MS

--- api-password-manager/src/main/java/com/martins/jean/api/password/manager/interfaces/controller/PasswordController.java
+++ api-password-manager/src/main/java/com/martins/jean/api/password/manager/interfaces/controller/PasswordController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/password-manager")
+@RequestMapping
 @Slf4j
 public class PasswordController {
 
