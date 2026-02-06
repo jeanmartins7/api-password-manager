0e90eb7b3bb3318c585ca256fcc2ed71bb1b096a
jeanverso <jeanmarcel8@gmail.com>
2026-02-06 11:24:38 +0000

    Migrate from javax to jakarta for Spring Boot 3 compatibility

--- src/main/java/com/martins/jean/api/password/manager/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/martins/jean/api/password/manager/exceptions/ResourceExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ResourceExceptionHandler {
